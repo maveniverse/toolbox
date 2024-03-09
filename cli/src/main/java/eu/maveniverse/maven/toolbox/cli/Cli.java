b0190f2ace6f47e2301e47ded36cea90a6b37ddb
Tamas Cservenak <tamas@cservenak.net>
2024-03-09 16:39:41 +0100

    A push

--- cli/src/main/java/eu/maveniverse/maven/toolbox/cli/Cli.java
+++ cli/src/main/java/eu/maveniverse/maven/toolbox/cli/Cli.java
@@ -2,6 +2,6 @@ package eu.maveniverse.maven.toolbox.cli;
 
 public class Cli {
     public static void main(String[] args) {
-        System.out.println("boo");
+        System.out.println("booo");
     }
 }

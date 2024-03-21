054c769452e7d56198df3cbd66723de52d0e6326
Tamas Cservenak <tamas@cservenak.net>
2024-03-21 13:58:54 +0100

    WIP

---
+++ maven-plugin/src/main/java/eu/maveniverse/maven/toolbox/plugin/ProjectMojoSupport.java
@@ -0,0 +1,82 @@
+/*
+ * Copyright (c) 2023-2024 Maveniverse Org.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+package eu.maveniverse.maven.toolbox.plugin;
+
+import eu.maveniverse.maven.mima.context.Context;
+import eu.maveniverse.maven.mima.context.ContextOverrides;
+import eu.maveniverse.maven.mima.context.Runtime;
+import eu.maveniverse.maven.mima.context.Runtimes;
+import eu.maveniverse.maven.toolbox.shared.Output;
+import eu.maveniverse.maven.toolbox.shared.ResolutionRoot;
+import eu.maveniverse.maven.toolbox.shared.Slf4jOutput;
+import eu.maveniverse.maven.toolbox.shared.ToolboxCommando;
+import java.util.stream.Collectors;
+import org.apache.maven.RepositoryUtils;
+import org.apache.maven.artifact.handler.manager.ArtifactHandlerManager;
+import org.apache.maven.execution.MavenSession;
+import org.apache.maven.plugin.AbstractMojo;
+import org.apache.maven.plugin.MojoExecutionException;
+import org.apache.maven.plugin.MojoFailureException;
+import org.apache.maven.plugins.annotations.Component;
+import org.apache.maven.plugins.annotations.Parameter;
+import org.apache.maven.project.MavenProject;
+import org.eclipse.aether.artifact.ArtifactTypeRegistry;
+import org.eclipse.aether.artifact.DefaultArtifact;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Support class for "project aware" Mojos.
+ */
+public abstract class ProjectMojoSupport extends AbstractMojo {
+    protected final Logger logger = LoggerFactory.getLogger(getClass());
+
+    protected final Output output = new Slf4jOutput(logger);
+
+    @Parameter(property = "verbose", defaultValue = "false", required = true)
+    protected boolean verbose;
+
+    @Component
+    private MavenProject mavenProject;
+
+    @Component
+    private MavenSession mavenSession;
+
+    @Component
+    private ArtifactHandlerManager artifactHandlerManager;
+
+    protected ResolutionRoot projectAsResolutionRoot() {
+        ArtifactTypeRegistry artifactTypeRegistry =
+                mavenSession.getRepositorySession().getArtifactTypeRegistry();
+        return ResolutionRoot.ofNotLoaded(new DefaultArtifact(
+                        mavenProject.getGroupId(),
+                        mavenProject.getArtifactId(),
+                        artifactHandlerManager
+                                .getArtifactHandler(mavenProject.getPackaging())
+                                .getExtension(),
+                        mavenProject.getVersion()))
+                .withDependencies(mavenProject.getDependencies().stream()
+                        .map(d -> RepositoryUtils.toDependency(d, artifactTypeRegistry))
+                        .collect(Collectors.toList()))
+                .withManagedDependencies(mavenProject.getDependencyManagement().getDependencies().stream()
+                        .map(d -> RepositoryUtils.toDependency(d, artifactTypeRegistry))
+                        .collect(Collectors.toList()))
+                .build();
+    }
+
+    @Override
+    public final void execute() throws MojoExecutionException, MojoFailureException {
+        Runtime runtime = Runtimes.INSTANCE.getRuntime();
+        try (Context context = runtime.create(ContextOverrides.create().build())) {
+            doExecute(ToolboxCommando.getOrCreate(runtime, context));
+        }
+    }
+
+    protected abstract void doExecute(ToolboxCommando toolboxCommando)
+            throws MojoExecutionException, MojoFailureException;
+}
--- maven-plugin/src/main/java/eu/maveniverse/maven/toolbox/plugin/TreeMojo.java
+++ maven-plugin/src/main/java/eu/maveniverse/maven/toolbox/plugin/TreeMojo.java
@@ -7,61 +7,23 @@
  */
 package eu.maveniverse.maven.toolbox.plugin;
 
-import eu.maveniverse.maven.mima.context.Context;
-import eu.maveniverse.maven.mima.context.ContextOverrides;
-import eu.maveniverse.maven.mima.context.Runtime;
-import eu.maveniverse.maven.mima.context.Runtimes;
 import eu.maveniverse.maven.toolbox.shared.ResolutionScope;
-import eu.maveniverse.maven.toolbox.shared.Slf4jOutput;
 import eu.maveniverse.maven.toolbox.shared.ToolboxCommando;
-import org.apache.maven.artifact.handler.manager.ArtifactHandlerManager;
-import org.apache.maven.plugin.AbstractMojo;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.apache.maven.plugin.MojoFailureException;
-import org.apache.maven.plugins.annotations.Component;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
-import org.apache.maven.project.MavenProject;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 @Mojo(name = "tree", threadSafe = true)
-public class TreeMojo extends AbstractMojo {
-    private final Logger logger = LoggerFactory.getLogger(getClass());
-
+public class TreeMojo extends ProjectMojoSupport {
     /**
      * The resolution scope to display, accepted values are "runtime", "compile", "test", etc.
      */
     @Parameter(property = "scope", defaultValue = "runtime", required = true)
     private String scope;
 
-    /**
-     * Set it {@code true} for verbose tree.
-     */
-    @Parameter(property = "verbose", defaultValue = "false", required = true)
-    private boolean verbose;
-
-    @Component
-    private MavenProject mavenProject;
-
-    @Component
-    private ArtifactHandlerManager artifactHandlerManager;
-
     @Override
-    public void execute() throws MojoExecutionException, MojoFailureException {
-        Runtime runtime = Runtimes.INSTANCE.getRuntime();
-        try (Context context = runtime.create(ContextOverrides.create().build())) {
-            ToolboxCommando.getOrCreate(runtime, context)
-                    .tree(
-                            ResolutionScope.parse(scope),
-                            MavenProjectHelper.toRoot(
-                                    mavenProject,
-                                    artifactHandlerManager,
-                                    context.repositorySystemSession().getArtifactTypeRegistry()),
-                            verbose,
-                            new Slf4jOutput(logger));
-        } catch (RuntimeException e) {
-            throw new MojoExecutionException(e.getCause());
-        }
+    protected void doExecute(ToolboxCommando toolboxCommando) throws MojoExecutionException, MojoFailureException {
+        toolboxCommando.tree(ResolutionScope.parse(scope), projectAsResolutionRoot(), verbose, output);
     }
 }

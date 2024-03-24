8e64f4f15887a9493a8dd2a4055d9f9d1e1f2110
Tamas Cservenak <tamas@cservenak.net>
2024-03-24 23:54:30 +0100

    Adjust resolve

--- toolbox/src/main/java/eu/maveniverse/maven/toolbox/plugin/mp/ResolveMojo.java
+++ toolbox/src/main/java/eu/maveniverse/maven/toolbox/plugin/mp/ResolveMojo.java
@@ -9,24 +9,17 @@ package eu.maveniverse.maven.toolbox.plugin.mp;
 
 import eu.maveniverse.maven.toolbox.plugin.MPMojoSupport;
 import eu.maveniverse.maven.toolbox.shared.Output;
-import eu.maveniverse.maven.toolbox.shared.ResolutionRoot;
-import eu.maveniverse.maven.toolbox.shared.ResolutionScope;
 import eu.maveniverse.maven.toolbox.shared.ToolboxCommando;
 import java.util.stream.Collectors;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
+import org.eclipse.aether.graph.Dependency;
 
 /**
- * Resolves transitively selected dependencies.
+ * Resolves selected dependencies.
  */
-@Mojo(name = "resolve-transitive", requiresProject = false, threadSafe = true)
-public class ResolveTransitiveMojo extends MPMojoSupport {
-    /**
-     * The resolution scope to resolve, accepted values are "runtime", "compile", "test", etc.
-     */
-    @Parameter(property = "scope", defaultValue = "runtime", required = true)
-    private String scope;
-
+@Mojo(name = "resolve", requiresProject = false, threadSafe = true)
+public class ResolveMojo extends MPMojoSupport {
     /**
      * The dependency matcher spec.
      */
@@ -53,14 +46,10 @@ public class ResolveTransitiveMojo extends MPMojoSupport {
 
     @Override
     protected boolean doExecute(Output output, ToolboxCommando toolboxCommando) throws Exception {
-        ResolutionRoot project = projectAsResolutionRoot();
-        return toolboxCommando.resolveTransitive(
-                ResolutionScope.parse(scope),
+        return toolboxCommando.resolve(
                 projectAsResolutionRoot().getDependencies().stream()
                         .filter(toolboxCommando.parseDependencyMatcherSpec(depSpec))
-                        .map(d -> ResolutionRoot.ofLoaded(d.getArtifact())
-                                .withManagedDependencies(project.getManagedDependencies())
-                                .build())
+                        .map(Dependency::getArtifact)
                         .collect(Collectors.toList()),
                 sources,
                 javadoc,

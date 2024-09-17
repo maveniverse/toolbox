8eb4836512f0ddede0e4d5020fba4ca7460f8c27
Tamas Cservenak <tamas@cservenak.net>
2024-09-17 16:02:54 +0200

    Convert all ITs

--- toolbox-cli-its/src/it/gav-identify-2/verify.groovy
+++
@@ -1,9 +0,0 @@
-/*
- * Copyright (c) 2023-2024 Maveniverse Org.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v2.0
- * which accompanies this distribution, and is available at
- * https://www.eclipse.org/legal/epl-v20.html
- */
-String buildLog = new File(basedir, 'build.log').text
-assert buildLog.contains('0235ba8b489512805ac13a8f9ea77a1ca5ebe3e8 (./aopalliance-1.0.jar) = aopalliance:aopalliance:jar:1.0')

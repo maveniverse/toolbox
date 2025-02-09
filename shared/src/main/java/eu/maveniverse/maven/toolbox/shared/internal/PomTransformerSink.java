d1953d8b056ccc5fe1c337bd40725758cbeea9ad
Tamas Cservenak <tamas@cservenak.net>
2025-02-09 17:25:37 +0100

    Reformat

--- shared/src/main/java/eu/maveniverse/maven/toolbox/shared/internal/PomTransformerSink.java
+++ shared/src/main/java/eu/maveniverse/maven/toolbox/shared/internal/PomTransformerSink.java
@@ -315,7 +315,8 @@ public final class PomTransformerSink implements Artifacts.Sink {
     @Override
     public void close() throws IOException {
         if (!applicableTransformations.isEmpty()) {
-            new PomTransformer(pom, StandardCharsets.UTF_8, PomTransformer.SimpleElementWhitespace.AUTODETECT_PREFER_EMPTY)
+            new PomTransformer(
+                            pom, StandardCharsets.UTF_8, PomTransformer.SimpleElementWhitespace.AUTODETECT_PREFER_EMPTY)
                     .transform(applicableTransformations);
         }
     }

b3b32c6f4ff98a3f9147299e240828ea37b39eaf
Tamas Cservenak <tamas@cservenak.net>
2025-02-12 22:11:17 +0100

    WIP

--- shared/src/main/java/eu/maveniverse/maven/toolbox/shared/internal/PomTransformerSink.java
+++ shared/src/main/java/eu/maveniverse/maven/toolbox/shared/internal/PomTransformerSink.java
@@ -19,12 +19,14 @@ import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.function.Consumer;
 import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.function.Supplier;
 import org.eclipse.aether.artifact.Artifact;
 import org.jdom2.Document;
+import org.jdom2.Element;
 import org.jdom2.JDOMException;
 import org.jdom2.input.SAXBuilder;
 import org.jdom2.located.LocatedJDOMFactory;
@@ -52,27 +54,181 @@ public final class PomTransformerSink implements Artifacts.Sink {
      */
     private final Transformation removeEmptyElements = ctx -> {};
 
+    /**
+     * Helper for GA equality.
+     */
+    private static boolean equalsGA(Artifact artifact, Element element) {
+        return Objects.equals(artifact.getGroupId(), element.getChildText("groupId", element.getNamespace()))
+                && Objects.equals(artifact.getArtifactId(), element.getChildText("artifactId", element.getNamespace()));
+    }
+
+    private static Consumer<TransformationContext> addOrSetProperty(String key, String value) {
+        return context -> {
+            Element project = context.getDocument().getRootElement();
+            if (project != null) {
+                Element properties = project.getChild("properties", project.getNamespace());
+                if (properties == null) {
+                    properties = new Element("properties", project.getNamespace());
+                    project.addContent(properties);
+                }
+                Element property = properties.getChild(key, properties.getNamespace());
+                if (property == null) {
+                    property = new Element(key, properties.getNamespace());
+                    properties.addContent(property);
+                }
+                property.setText(value);
+            }
+        };
+    }
+
     public static Function<Artifact, Consumer<TransformationContext>> updateManagedPlugin(boolean upsert) {
         return a -> context -> {
-            throw new RuntimeException("not implemented");
+            Element project = context.getDocument().getRootElement();
+            if (project != null) {
+                Element build = project.getChild("build", project.getNamespace());
+                if (upsert && build == null) {
+                    build = new Element("build", project.getNamespace());
+                    project.addContent(project.addContent(build));
+                }
+                if (build != null) {
+                    Element pluginManagement = project.getChild("pluginManagement", project.getNamespace());
+                    if (upsert && pluginManagement == null) {
+                        pluginManagement = new Element("pluginManagement", project.getNamespace());
+                        build.addContent(pluginManagement);
+                    }
+                    if (pluginManagement != null) {
+                        Element plugins = pluginManagement.getChild("plugins", pluginManagement.getNamespace());
+                        if (upsert && plugins == null) {
+                            plugins = new Element("plugins", pluginManagement.getNamespace());
+                            pluginManagement.addContent(plugins);
+                        }
+                        if (plugins != null) {
+                            Element toUpdate = null;
+                            for (Element plugin : plugins.getChildren("plugin", plugins.getNamespace())) {
+                                if (equalsGA(a, plugin)) {
+                                    toUpdate = plugin;
+                                    break;
+                                }
+                            }
+                            if (upsert && toUpdate == null) {
+                                toUpdate = new Element("plugin", plugins.getNamespace());
+                                toUpdate.addContent(
+                                        new Element("groupId", plugins.getNamespace()).setText(a.getGroupId()));
+                                toUpdate.addContent(
+                                        new Element("artifactId", plugins.getNamespace()).setText(a.getArtifactId()));
+                                toUpdate.addContent(
+                                        new Element("version", plugins.getNamespace()).setText(a.getVersion()));
+                                plugins.addContent(toUpdate);
+                                return;
+                            }
+                            if (toUpdate != null) {
+                                Element version = toUpdate.getChild("version", toUpdate.getNamespace());
+                                if (version != null) {
+                                    String versionValue = version.getText();
+                                    if (versionValue.startsWith("${") && versionValue.endsWith("}")) {
+                                        String propertyKey = versionValue.substring(2, versionValue.length() - 1);
+                                        addOrSetProperty(propertyKey, a.getVersion())
+                                                .accept(context);
+                                    } else {
+                                        version.setText(a.getVersion());
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         };
     }
 
     public static Function<Artifact, Consumer<TransformationContext>> deleteManagedPlugin() {
         return a -> context -> {
-            throw new RuntimeException("not implemented");
+            Element project = context.getDocument().getRootElement();
+            if (project != null) {
+                Element build = project.getChild("build", project.getNamespace());
+                if (build != null) {
+                    Element pluginManagement = project.getChild("pluginManagement", project.getNamespace());
+                    if (pluginManagement != null) {
+                        Element plugins = pluginManagement.getChild("plugins", pluginManagement.getNamespace());
+                        if (plugins != null) {
+                            for (Element plugin : plugins.getChildren("plugin", plugins.getNamespace())) {
+                                if (equalsGA(a, plugin)) {
+                                    plugin.detach();
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         };
     }
 
     public static Function<Artifact, Consumer<TransformationContext>> updatePlugin(boolean upsert) {
         return a -> context -> {
-            throw new RuntimeException("not implemented");
+            Element project = context.getDocument().getRootElement();
+            if (project != null) {
+                Element build = project.getChild("build", project.getNamespace());
+                if (upsert && build == null) {
+                    build = new Element("build", project.getNamespace());
+                    project.addContent(project.addContent(build));
+                }
+                if (build != null) {
+                    Element plugins = build.getChild("plugins", build.getNamespace());
+                    if (upsert && plugins == null) {
+                        plugins = new Element("plugins", build.getNamespace());
+                        build.addContent(plugins);
+                    }
+                    if (plugins != null) {
+                        Element toUpdate = null;
+                        for (Element plugin : plugins.getChildren("plugin", plugins.getNamespace())) {
+                            if (equalsGA(a, plugin)) {
+                                toUpdate = plugin;
+                                break;
+                            }
+                        }
+                        if (upsert && toUpdate == null) {
+                            toUpdate = new Element("plugin", plugins.getNamespace());
+                            toUpdate.addContent(new Element("groupId", plugins.getNamespace()).setText(a.getGroupId()));
+                            toUpdate.addContent(
+                                    new Element("artifactId", plugins.getNamespace()).setText(a.getArtifactId()));
+                            toUpdate.addContent(new Element("version", plugins.getNamespace()).setText(a.getVersion()));
+                            plugins.addContent(toUpdate);
+                            return;
+                        }
+                        if (toUpdate != null) {
+                            Element version = toUpdate.getChild("version", plugins.getNamespace());
+                            if (version != null) {
+                                String versionValue = version.getText();
+                                if (versionValue.startsWith("${") && versionValue.endsWith("}")) {
+                                    String propertyKey = versionValue.substring(2, versionValue.length() - 1);
+                                    addOrSetProperty(propertyKey, a.getVersion())
+                                            .accept(context);
+                                } else {
+                                    version.setText(a.getVersion());
+                                }
+                            } else {
+                                updateManagedPlugin(upsert).apply(a).accept(context);
+                            }
+                        }
+                    }
+                }
+            }
         };
     }
 
     public static Function<Artifact, Consumer<TransformationContext>> deletePlugin() {
         return a -> context -> {
-            throw new RuntimeException("not implemented");
+            Element project = context.getDocument().getRootElement();
+            if (project != null) {
+                Element plugins = project.getChild("plugins", project.getNamespace());
+                if (plugins != null) {
+                    for (Element plugin : plugins.getChildren("plugin", plugins.getNamespace())) {
+                        if (equalsGA(a, plugin)) {
+                            plugin.detach();
+                        }
+                    }
+                }
+            }
         };
     }
 
@@ -84,7 +240,21 @@ public final class PomTransformerSink implements Artifacts.Sink {
 
     public static Function<Artifact, Consumer<TransformationContext>> deleteManagedDependency() {
         return a -> context -> {
-            throw new RuntimeException("not implemented");
+            Element project = context.getDocument().getRootElement();
+            if (project != null) {
+                Element dependencyManagement = project.getChild("dependencyManagement", project.getNamespace());
+                if (dependencyManagement != null) {
+                    Element dependencies =
+                            dependencyManagement.getChild("dependencies", dependencyManagement.getNamespace());
+                    if (dependencies != null) {
+                        for (Element dependency : dependencies.getChildren("dependency", dependencies.getNamespace())) {
+                            if (equalsGA(a, dependency)) {
+                                dependency.detach();
+                            }
+                        }
+                    }
+                }
+            }
         };
     }
 
@@ -96,7 +266,17 @@ public final class PomTransformerSink implements Artifacts.Sink {
 
     public static Function<Artifact, Consumer<TransformationContext>> deleteDependency() {
         return a -> context -> {
-            throw new RuntimeException("not implemented");
+            Element project = context.getDocument().getRootElement();
+            if (project != null) {
+                Element dependencies = project.getChild("dependencies", project.getNamespace());
+                if (dependencies != null) {
+                    for (Element dependency : dependencies.getChildren("dependency", dependencies.getNamespace())) {
+                        if (equalsGA(a, dependency)) {
+                            dependency.detach();
+                        }
+                    }
+                }
+            }
         };
     }
 
@@ -211,12 +391,7 @@ public final class PomTransformerSink implements Artifacts.Sink {
             } catch (JDOMException e) {
                 throw new IOException(e);
             }
-            TransformationContext context = new TransformationContext() {
-                @Override
-                public Document getDocument() {
-                    return document;
-                }
-            };
+            TransformationContext context = () -> document;
             for (Consumer<TransformationContext> transformation : applicableTransformations) {
                 transformation.accept(context);
             }

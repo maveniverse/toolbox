2178ca6978a2cd8e8d4c0fbcb17d3b25e9ddf39b
Tamas Cservenak <tamas@cservenak.net>
2025-03-20 15:43:51 +0100

    Logical mistake

--- shared/src/main/java/eu/maveniverse/maven/toolbox/shared/output/LoggerOutput.java
+++ shared/src/main/java/eu/maveniverse/maven/toolbox/shared/output/LoggerOutput.java
@@ -23,9 +23,10 @@ public final class LoggerOutput extends OutputSupport {
 
     @Override
     public void warn(String message, Object... params) {
-        if (!isShowErrors() && params.length > 0 || params[params.length - 1] instanceof Throwable) {
-            Object[] paramsWithoutThrowable = Arrays.copyOf(params, params.length - 1);
-            output.warn(message + " " + ((Throwable) params[params.length - 1]).getMessage(), paramsWithoutThrowable);
+        if (!isShowErrors() && params.length > 0 && params[params.length - 1] instanceof Throwable) {
+            output.warn(
+                    message + " " + ((Throwable) params[params.length - 1]).getMessage(),
+                    Arrays.copyOf(params, params.length - 1));
         } else {
             output.warn(message, params);
         }
@@ -33,9 +34,10 @@ public final class LoggerOutput extends OutputSupport {
 
     @Override
     public void error(String message, Object... params) {
-        if (!isShowErrors() && params.length > 0 || params[params.length - 1] instanceof Throwable) {
-            Object[] paramsWithoutThrowable = Arrays.copyOf(params, params.length - 1);
-            output.error(message + " " + ((Throwable) params[params.length - 1]).getMessage(), paramsWithoutThrowable);
+        if (!isShowErrors() && params.length > 0 && params[params.length - 1] instanceof Throwable) {
+            output.error(
+                    message + " " + ((Throwable) params[params.length - 1]).getMessage(),
+                    Arrays.copyOf(params, params.length - 1));
         } else {
             output.error(message, params);
         }

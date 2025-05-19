67045245c845bd21d520be6e4e86bb25fdb2bca4
Tamas Cservenak <tamas@cservenak.net>
2025-05-19 17:04:57 +0200

    Add IT for new mojo

---
+++ it/toolbox-plugin-its/src/it/versions/verify.groovy
@@ -0,0 +1,17 @@
+/*
+ * Copyright (c) 2023-2024 Maveniverse Org.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+String buildLog = new File(basedir, 'build.log').text
+assert !buildLog.contains('ERROR')
+
+String pom = new File(basedir, 'pom.xml').text
+String expectedPom = new File(basedir, 'expected-pom.xml').text
+assert pom != null && expectedPom != null && pom.equals(expectedPom)
+
+String extensionsXml = new File(basedir, '.mvn/extensions.xml').text
+String originalExtensionsXml = new File(basedir, '.mvn/original-extensions.xml').text
+assert extensionsXml != null && originalExtensionsXml != null && !extensionsXml.equals(originalExtensionsXml)

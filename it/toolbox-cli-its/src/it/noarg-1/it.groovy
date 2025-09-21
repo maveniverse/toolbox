3499669a0e4da15ee3a938b90f3b6823dc8fe7b6
Tamas Cservenak <tamas@cservenak.net>
2025-09-21 11:45:56 +0200

    Add IT

---
+++ it/toolbox-cli-its/src/it/noarg-1/it.groovy
@@ -0,0 +1,20 @@
+/*
+ * Copyright (c) 2023-2024 Maveniverse Org.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
+StringBuilder out = new StringBuilder()
+StringBuilder err = new StringBuilder()
+String[] args = ["${java}", '-jar', "${cli}", "-e"]
+
+ProcessBuilder proc = new ProcessBuilder(args)
+Process process = proc.start()
+process.consumeProcessOutput(out, err)
+process.getOutputStream().flush()
+process.getOutputStream().close()
+process.waitFor()
+
+assert !err.toString().contains("NullPointerException")

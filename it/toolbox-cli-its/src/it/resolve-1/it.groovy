9f5d3b0b3fe2d3e5942982a247d5534589ffb95e
Tamas Cservenak <tamas@cservenak.net>
2026-01-23 16:39:00 +0100

    Add simple IT

---
+++ it/toolbox-cli-its/src/it/resolve-1/it.groovy
@@ -0,0 +1,18 @@
+/*
+ * Copyright (c) 2023-2024 Maveniverse Org.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v2.0
+ * which accompanies this distribution, and is available at
+ * https://www.eclipse.org/legal/epl-v20.html
+ */
+
+StringBuilder out = new StringBuilder()
+StringBuilder err = new StringBuilder()
+String[] args = ["${java}", '-jar', "${cli}", "-e", "resolve", "junit:junit:4.13.2", "--poms"]
+
+ProcessBuilder proc = new ProcessBuilder(args)
+Process process = proc.start()
+process.consumeProcessOutput(out, err)
+process.waitFor()
+
+assert out.contains("SHA-512: abf1cf90ab6a525ae0cfa5235563b00bc6ef07c59f8cdd5c5495ea8b14941b5803a3f7adffaa36ec37152a7904a10e04939c0d11b48115f1943a1606cc5066c0")

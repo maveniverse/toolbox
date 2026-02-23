34d0bd0edd4761b3e4ef646b30bf022e07ef69c9
Tamas Cservenak <tamas@cservenak.net>
2026-02-23 12:35:03 +0100

    Update parent 50 license header changes (#341)

--- it/toolbox-cli-its/src/it/dm-tree-1/it.groovy
+++ it/toolbox-cli-its/src/it/dm-tree-1/it.groovy
@@ -1,11 +1,10 @@
 /*
- * Copyright (c) 2023-2024 Maveniverse Org.
+ * Copyright (c) 2023-2026 Maveniverse Org.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v2.0
  * which accompanies this distribution, and is available at
  * https://www.eclipse.org/legal/epl-v20.html
  */
-
 StringBuilder out = new StringBuilder()
 StringBuilder err = new StringBuilder()
 String[] args = ["${java}", '-jar', "${cli}", "-e", "dm-tree", "org.springframework.boot:spring-boot-dependencies:3.3.3"]
